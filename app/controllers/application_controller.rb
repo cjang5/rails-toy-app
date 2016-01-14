e0117d74fb685bc722c223b58a07c5c67190ce09
cjang5 <chrismangggg@gmail.com>
2016-01-13 21:01:06 -0500

    Added hellow orld message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # make it say "hello" just for initial Heroku deployment
+  def hello
+  	render :text "Hello again, world!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  # make root route 
+  root 'application#hello'
 end
